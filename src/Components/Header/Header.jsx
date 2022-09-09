be3acf2ddeb32a20a216a184b0120a24f5cc18dd
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-09 13:57:43 +0700

    memasukan profile image asset ke logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import styles from "../../styles/Header.module.css";
 import Hamburger from "./Hamburger";
+import profileImage from "../../images/profile.png";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <div>{/* <img src="" alt="" /> */}</div>
+          <div>
+            <img src={profileImage} alt="profile" />
+          </div>
           <h1>AhmadDedenSetiawan</h1>
         </div>
         <Hamburger />
