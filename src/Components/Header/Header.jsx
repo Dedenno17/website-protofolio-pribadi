c8e7a130a66d8b58f5ad4b98f5eea64e51510c22
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-09 09:04:26 +0700

    membuat Header component & mereset css

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h1>Ahmad Deden Setiawan</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
