478da723d0a5b72ee25b6c455c1caf1e5a009e8a
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-09 10:19:11 +0700

    membuat Header module css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,11 @@ import styles from "../../styles/Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <div>{/* <img src="" alt="" /> */}</div>
+          <h1>Ahmad Deden Setiawan</h1>
         </div>
-        <h1>Ahmad Deden Setiawan</h1>
       </div>
     </header>
   );
