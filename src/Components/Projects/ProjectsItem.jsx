28fe6f8797bfe9cd7c3e722b113ba802e927117b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-13 09:13:40 +0700

    membuat projectsitem component & menambahkan asset icon caret right

---
+++ src/Components/Projects/ProjectsItem.jsx
@@ -0,0 +1,16 @@
+const ProjectsItem = (props) => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <h3>Project Name</h3>
+        <a href="/">
+          <p>View details</p>
+          <span></span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsItem;
