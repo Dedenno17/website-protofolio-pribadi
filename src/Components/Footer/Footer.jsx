f2b94887cfa12dde892784299290adc1f7128f6e
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-17 20:52:03 +0700

    menyambungkan link navigasi yang ada di footer & mengatur padding nya sedikit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,16 +21,16 @@ const Footer = () => {
         <div className={styles.links}>
           <ul>
             <li>
-              <a href="/">Home</a>
+              <a href="#home">Home</a>
             </li>
             <li>
-              <a href="/">Projects</a>
+              <a href="#projects">Projects</a>
             </li>
             <li>
-              <a href="/">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
             <li>
-              <a href="/">About</a>
+              <a href="#about">About</a>
             </li>
           </ul>
           <ul>
