cf7842e4bcdff81753dd5eb3001faa37cecb6b3f
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-18 19:09:29 +0700

    menambahkan scroll behavior pada index.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import About from "./Components/About/About";
 
+import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
