5f3480d0348579799cf131089b6b6cc350549c2c
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-19 13:17:55 +0700

    membuat header menjadi fixed ketika discroll kebawah

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState, useCallback } from "react";
 import styles from "../../styles/Header.module.css";
 import Hamburger from "./Hamburger";
 import profileImage from "../../images/profile.png";
 
 const Header = (props) => {
+  const [scrollY, setScrollY] = useState(null);
+
+  const scrollHandler = useCallback(function () {
+    setScrollY(this.scrollY);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler, true);
+    };
+  }, [scrollHandler]);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={
+        scrollY > 200 ? `${styles.header} ${styles.fixed}` : `${styles.header}`
+      }
+    >
       <div className={styles.container}>
         <div className={styles.logo}>
           <div>
