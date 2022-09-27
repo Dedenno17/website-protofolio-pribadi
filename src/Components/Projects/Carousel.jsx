d674964da297e5118ea23af24ef629e75280829e
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-27 11:16:02 +0700

    membuat logic agar caraousel terhubung dengan custom navigation & mengubah sedikit props autoplay

--- src/Components/Projects/Carousel.jsx
+++ src/Components/Projects/Carousel.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
+import { Autoplay, Navigation } from "swiper";
 
 import "swiper/css";
 import "swiper/css/autoplay";
@@ -9,18 +9,33 @@ import angleRight from "../../images/angle-right-solid.svg";
 import angleLeft from "../../images/angle-left-solid.svg";
 
 export default function Carousel(props) {
+  const prevBtnRef = useRef(null);
+  const nextBtnRef = useRef(null);
+
   return (
     <div className={styles.carousel}>
-      <span>
+      <span ref={prevBtnRef}>
         <img src={angleLeft} alt="left"></img>
       </span>
       <Swiper
-        modules={[Autoplay]}
+        modules={[Autoplay, Navigation]}
         speed={800}
         spaceBetween={30}
         slidesPerView={2}
+        navigation={{
+          prevEl: prevBtnRef.current,
+          nextEl: nextBtnRef.current,
+        }}
+        onInit={(swiper) => {
+          swiper.params.navigation.prevEl = prevBtnRef.current;
+          swiper.params.navigation.nextEl = nextBtnRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
         loop
-        autoplay
+        autoplay={{
+          disableOnInteraction: false,
+        }}
         className={styles.swiper}
       >
         {props.dataProjects.map((item, i) => (
@@ -47,7 +62,7 @@ export default function Carousel(props) {
           </SwiperSlide>
         ))}
       </Swiper>
-      <span>
+      <span ref={nextBtnRef}>
         <img src={angleRight} alt="right"></img>
       </span>
     </div>
