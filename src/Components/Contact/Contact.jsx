4021681360ff1f81f5c6d29a06f0eaf4daf36c6b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-28 08:40:57 +0700

    memperbaiki style Contact ketika screen lg

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,15 +4,17 @@ import Button from "../UI/Button";
 const Contact = () => {
   return (
     <div className={styles.contact} id="contact">
-      <h2>Contact</h2>
-      <span>
-        <a href="https://wa.me/88214455616" target="__blank">
-          Whats app
-        </a>
-        <a href="mailto:ahmaddedensetiawan98@gmail.com">
-          <Button type="button">Email me</Button>
-        </a>
-      </span>
+      <div>
+        <h2>Contact</h2>
+        <span>
+          <a href="https://wa.me/88214455616" target="__blank">
+            Whats app
+          </a>
+          <a href="mailto:ahmaddedensetiawan98@gmail.com">
+            <Button type="button">Email me</Button>
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
