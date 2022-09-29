25ecc2f25cf8bed6151f5514afd448c2719cc97b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-09-29 10:12:22 +0700

    mengatur style about ketika screen x

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,20 +6,22 @@ import resume from "../../images/Resume.pdf";
 const About = () => {
   return (
     <div className={styles.about} id="about">
-      <div>
-        <h2>About me</h2>
-        <p>
-          Create an attractive & interactive website display, by mastering
-          several tools that support the development of a web application & has
-          been certified by parties who have credibility on web development
-          field{" "}
-        </p>
-        <a href={resume} download="resume">
-          <Button type="button">Download CV</Button>
-        </a>
-      </div>
-      <div>
-        <img src={profile} alt="profile" />
+      <div className={styles.container}>
+        <div>
+          <h2>About me</h2>
+          <p>
+            Create an attractive & interactive website display, by mastering
+            several tools that support the development of a web application &
+            has been certified by parties who have credibility on web
+            development field{" "}
+          </p>
+          <a href={resume} download="resume">
+            <Button type="button">Download CV</Button>
+          </a>
+        </div>
+        <div>
+          <img src={profile} alt="profile" />
+        </div>
       </div>
     </div>
   );
