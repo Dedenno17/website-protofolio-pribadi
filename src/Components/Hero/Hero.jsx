74c92a9da0593bc7e7497f9dfe62fc520ce3b112
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-03 09:13:14 +0700

    memberi animasi pada section hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,6 +4,29 @@ import Button from "../UI/Button";
 import { motion } from "framer-motion";
 
 const Hero = () => {
+  const descVariant = {
+    hidden: {
+      x: -50,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.5,
+        delayChildren: 1.5,
+      },
+    },
+  };
+
+  const dragValue = {
+    top: 10,
+    right: 10,
+    left: -40,
+    bottom: 10,
+  };
+
   return (
     <div className={styles.hero} id="home">
       <div className={styles.image}>
@@ -12,26 +35,57 @@ const Hero = () => {
           animate={{ opacity: 1 }}
           transition={{
             duration: 0.7,
-            delay: 0.2,
           }}
         ></motion.div>
-        <img src={profile} alt="hero profile" />
-        <div></div>
-        <div></div>
+        <motion.img
+          src={profile}
+          alt="hero profile"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.7,
+            delay: 0.3,
+          }}
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.7,
+          }}
+          drag
+          dragConstraints={dragValue}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.7,
+          }}
+          drag
+          dragConstraints={dragValue}
+        ></motion.div>
       </div>
-      <div className={styles.desc}>
-        <h2>Hello, I’m Ahmad Deden Setiawan</h2>
-        <div>
+      <motion.div
+        className={styles.desc}
+        variants={descVariant}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.h2 variants={descVariant}>
+          Hello, I’m Ahmad Deden Setiawan
+        </motion.h2>
+        <motion.div variants={descVariant}>
           <p>Front-End Web Developer</p>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p variants={descVariant}>
           Indonesian developer who build beautiful and interactive web
           applications using popular frameworks and tools
-        </p>
-        <a href="#contact">
+        </motion.p>
+        <motion.a href="#contact" variants={descVariant}>
           <Button type="button">Contact</Button>
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </div>
   );
 };
