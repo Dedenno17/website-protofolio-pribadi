7e22e4ade5d0b0b34c30171650bf6da0ccf89fef
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-04 09:20:59 +0700

    memberi animasi pada contact section saat dia terkena view

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,19 +1,30 @@
 import styles from "../../styles/Contact.module.css";
+import { motion } from "framer-motion";
 import Button from "../UI/Button";
 
 const Contact = () => {
   return (
     <div className={styles.contact} id="contact">
       <div>
-        <h2>Contact</h2>
-        <span>
+        <motion.h2
+          initial={{ x: -90, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+        >
+          Contact
+        </motion.h2>
+        <motion.span
+          initial={{ x: 90, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+        >
           <a href="https://wa.me/88214455616" target="__blank">
             Whats app
           </a>
           <a href="mailto:ahmaddedensetiawan98@gmail.com">
             <Button type="button">Email me</Button>
           </a>
-        </span>
+        </motion.span>
       </div>
     </div>
   );
